00d2a438e84373c2a048b7e90f891cb3b30860e1
agungsulaksonor <119735605+agungsulaksonor@users.noreply.github.com>
2023-01-20 11:00:51 +0700

    Add files via upload

---
+++ UAS_ROBOTICS/my_supervisor/controllers/supervisor_controller/build/release/supervisor_controller.d
@@ -0,0 +1,6 @@
+build/release/supervisor_controller.o: supervisor_controller.c \
+ D:\Webots/include/controller/c/webots/robot.h \
+ D:\Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/nodes.h \
+ D:\Webots/include/controller/c/webots/supervisor.h \
+ D:\Webots/include/controller/c/webots/contact_point.h
